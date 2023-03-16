02a4d4b388e30e3ba341cce89f9c3effa441b728
saki <16546851+sakiko-2@users.noreply.github.com>
2023-03-16 18:17:42 +1300

    Add check for key and value

--- lru.js
+++ lru.js
@@ -4,43 +4,65 @@ export class LRUCache {
     this.cache = [];
   }
 
+  isKeyValid(key) {
+    if (key != undefined | typeof key === "number" | key >= 0 | key <= 1000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isValuevalid(value) {
+    if (value != undefined | typeof value === "number" | value >= 0 | value <= 100000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   getObject(arr, key) {
     return arr.filter((obj) => Object.keys(obj).includes(key.toString()))[0];
   }
 
   get(key) {
-    const obj = this.getObject(this.cache, key);
+    if (this.isKeyValid) {
+      const obj = this.getObject(this.cache, key);
 
-    if (obj !== undefined) {
-      this.cache.push(this.cache.splice(this.cache.indexOf(obj), 1)[0]);
-      return obj[key];
-    } else {
-      return -1;
+      if (obj !== undefined) {
+        this.cache.push(this.cache.splice(this.cache.indexOf(obj), 1)[0]);
+        return obj[key];
+      } else {
+        return -1;
+      }
     }
   }
 
   put(key, value) {
-    const createObject = (key, value) => {
-      const obj = {};
-      obj[key] = value;
-      this.cache.push(obj);
-    };
+    if (this.isKeyValid && this.isValuevalid) {
+      const createObject = (key, value) => {
+        const obj = {};
+        obj[key] = value;
+        this.cache.push(obj);
+      };
 
-    if (this.cache.length === this.capacity) {
-      this.cache.shift();
+      if (this.cache.length === this.capacity) {
+        this.cache.shift();
+      }
+
+      createObject(key, value);
     }
-
-    createObject(key, value);
   }
 
   delete(key) {
-    const obj = this.getObject(this.cache, key);
+    if (this.isKeyValid) {
+      const obj = this.getObject(this.cache, key);
 
-    if (obj !== undefined) {
-      this.cache.splice(this.cache.indexOf(obj), 1);
-      return key;
-    } else {
-      return -1;
+      if (obj !== undefined) {
+        this.cache.splice(this.cache.indexOf(obj), 1);
+        return key;
+      } else {
+        return -1;
+      }
     }
   }
 }
