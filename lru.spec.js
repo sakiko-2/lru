85c925b1e34524241ab8dbfb4b88b74c6f5be5a1
saki <16546851+sakiko-2@users.noreply.github.com>
2023-03-16 16:59:34 +1300

    Fix typo

--- lru.spec.js
+++ lru.spec.js
@@ -1,7 +1,7 @@
 import {LRUCache} from "./lru";
 
 describe("lru", () => {
-  test ("get(1) returns 1", () => {
+  test("get(1) returns 1", () => {
     const cache = new LRUCache(2);
     cache.put(1, 1);
     cache.put(2, 2);
